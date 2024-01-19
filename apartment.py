1e2d05095756931696cf2228fb5a9c208a1f52bd
kyunj <133327199+kyunj@users.noreply.github.com>
2024-01-19 17:10:11 +0900

    Update apartment.py

--- apartment.py
+++ apartment.py
@@ -83,7 +83,7 @@ import time
 latest_iteration = st.empty()
 bar = st.progress(0)
 
-for i in range(50):
+for i in range(100):
 # Update the progress bar with each iteration.
     latest_iteration.text(f'Iteration {i+1}')
     bar.progress(i + 2)
