c662861d07c03d3219f1ec8317af200ee26b2942
kyunj <133327199+kyunj@users.noreply.github.com>
2024-01-19 17:10:43 +0900

    Update apartment.py

--- apartment.py
+++ apartment.py
@@ -86,7 +86,7 @@ bar = st.progress(0)
 for i in range(100):
 # Update the progress bar with each iteration.
     latest_iteration.text(f'Iteration {i+1}')
-    bar.progress(i + 2)
+    bar.progress(i + 1)
     time.sleep(0.025)
   # 0.05 초 마다 1씩증가
     #st.balloons()
