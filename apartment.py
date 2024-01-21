e1fb14a1865d4e87ede806ef4ab289232a3de62b
kyunj <133327199+kyunj@users.noreply.github.com>
2024-01-21 20:20:54 +0900

    Update apartment.py

--- apartment.py
+++ apartment.py
@@ -114,11 +114,10 @@ if my_df.empty:
     ax = sns.lineplot(x=df['gu'], y=dff['cost'].mean(), linewidth=1, color='red', label='서울시 평균 관리비(원)')
     fig = plt.legend()
     fig = plt.xticks(rotation=45)
-    #fig = plt.text('강북구', dff['cost'].mean()-2000, '%.0f' % dff['cost'].mean(), ha='right', va='bottom', size=10)
+    fig = plt.text('강북구', dff['cost'].mean()-2000, '%.0f' % dff['cost'].mean(), ha='right', va='bottom', size=10)
 
     # Streamlit에 그래프를 표시
-    st.pyplot()
-    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot(showPyplotGlobalUse =false)
 else:  
   col1, col2,col3 = st.columns(3)
   col1.metric(label = '구 평균 관리비(단위:만원)', value = round(my_df_1['cost'].mean() / 10000, 3),
